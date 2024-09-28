15e090f4b843c272ae945435af3bc0893754e9e4
Sjaiod <delwar.saj2008@gmail.com>
2024-09-28 16:05:05 +0600

    dd

--- server/urls.py
+++ server/urls.py
@@ -2,8 +2,10 @@
 from django.contrib import admin
 from django.urls import path,include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from .views import HOmepage
 
 urlpatterns = [
+    path('', HOmepage.as_view(), name='home'),
     path('admin/', admin.site.urls),
     path('api/member/', include('members.urls')),
 ]
