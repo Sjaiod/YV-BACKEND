2e245da6072a846cf28c9fa27d90af19dee22131
Sjaiod <delwar.saj2008@gmail.com>
2024-09-28 16:02:07 +0600

    asd

---
+++ server/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny,IsAuthenticated
+
+
+class HOmepage(APIView):
+    permission_classes=[AllowAny]
+    def get(self, request):
+        return Response({"message":"Welcome to the Yoga Village API"})
