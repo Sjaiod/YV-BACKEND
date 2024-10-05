05044dc578309aa0110161c319f7504b5036bff6
Sjaiod <delwar.saj2008@gmail.com>
2024-10-05 12:03:59 +0600

    sda

--- server/settings.py
+++ server/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'members', 
     'whitenoise.runserver_nostatic',
     'corsheaders',
+    'events'
       # your app
 
     
--- server/urls.py
+++ server/urls.py
@@ -1,13 +1,14 @@
-
 from django.contrib import admin
 from django.urls import path,include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from .views import HOmepage
 
+
 urlpatterns = [
     path('', HOmepage.as_view(), name='welcome'),
     path('admin/', admin.site.urls),
     path('api/member/', include('members.urls')),
+    path('api/', include('events.urls')),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
